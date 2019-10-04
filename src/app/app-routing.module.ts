3670fb8738eed37047fec56cdde2ef17b009afef
Luis Osorio <losorio@consejodeauditoria.gob.cl>
2019-10-04 11:25:36 -0300

    Listo para produccion

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { Pagina2Component } from './pages/pagina2/pagina2.component';
+import { Pagina3Component } from './pages/pagina3/pagina3.component';
+
+
+const routes: Routes = [
+  {path:'pagina1', component:Pagina1Component},
+  {path:'pagina2', component:Pagina2Component},
+  {path:'pagina3', component:Pagina3Component},
+  {path:'**',pathMatch:'full',redirectTo:'pagina1'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
